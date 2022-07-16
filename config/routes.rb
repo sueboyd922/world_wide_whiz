5184f466afe675e8295faa07076dd35051056142
Susan Boyd <sueboyd922@gmail.com>
2022-07-16 17:12:18 -0400

    Feat: Route to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get "/", to: "home#index"
   get '/auth/google_oauth2/callback', to: 'users#show'
+
+
 end
