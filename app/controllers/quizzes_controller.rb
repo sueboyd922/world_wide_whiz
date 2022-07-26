fc9a98f1c462c04050e52c6a0611f6dbe0ae1ecf
Susan Boyd <sueboyd922@gmail.com>
2022-07-26 19:10:51 -0400

    New Quiz created

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -4,6 +4,13 @@ class QuizzesController < ApplicationController
   end
 
   def create
-    
+    quiz = Quiz.new(quiz_params)
+    quiz.user = current_user
+    quiz.save
+  end
+
+  private
+  def quiz_params
+    params.permit(:region, :level)
   end
 end
