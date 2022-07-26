ac7ad477cf18cab3cf746330c6a9650cabdd93dc
Susan Boyd <sueboyd922@gmail.com>
2022-07-26 10:19:13 -0400

    Trying to set up testing with oauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/", to: "home#index"
-  get '/auth/google_oauth2/callback', to: 'users#show'
+  get '/auth/google_oauth2/callback', to: 'users#create'
+  get "/dashboard", to: 'users#show'
 
 
 end
--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'home page' do
+  before do
+    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:google_oauth2]
+  end
+
   it 'has a button to login/register with google' do
     visit '/'
 
     expect(page).to have_button("Sign in with Google")
+    # click_on("Sign in with Google")
+    # require "pry"; binding.pry
   end
 end
