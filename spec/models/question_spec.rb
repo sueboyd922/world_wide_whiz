927b3ba59b0ffd0ea139c30d5bab4bff9f3e98a2
Susan Boyd <sueboyd922@gmail.com>
2022-07-26 18:59:53 -0400

    Test validations and relationships for Quiz and Question

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of (:question) }
+    it { should validate_presence_of (:answer) }
+  end
+
+  describe 'relationships' do
+    it { should belong_to (:quiz) }
+    it { should have_one(:user).through(:quiz) }
+  end
 end
--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Quiz, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it { should validate_presence_of (:level) }
+    it { should validate_presence_of (:region) }
+    it { should define_enum_for(:level) }
+    it { should define_enum_for(:region) }
+  end
+
+  describe 'relationships' do
+    it { should have_many(:questions) }
+    it { should belong_to(:user) }
+  end
 end
